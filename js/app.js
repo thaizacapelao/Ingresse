a38b3069cbd0961c6ee98f8fb92ed985d0e6c755
Thaiza Capelão <thaizacapelao@gmail.com>
2024-08-15 16:59:24 -0400

    att

--- js/app.js
+++ js/app.js
@@ -1,32 +1,46 @@
-let totalGeral = 0;
-limpar();
 
-function adicionar() {
-  // recuperar valores, nome do produto, quantidade e valor
-  let produto = document.getElementById("produto").value;
-  let nomeProduto = produto.split("-")[0];
-  let valorUnitario = produto.split("R$")[1];
-  let quantidade = document.getElementById("quantidade").value;
-  // calcular o preço, o nosso subtotal
-  let preco = quantidade * valorUnitario;
-  let carrinho = document.getElementById("lista-produtos");
-  // adicionar no carrinho
-  carrinho.innerHTML =
-    carrinho.innerHTML +
-    `<section class="carrinho__produtos__produto">
-<span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
-</section>`;
-  // atualizar o valor total da compra
+let qntPista = 100;
+let qntCadeiraSuperior = 200;
+let qntCadeiraInferior = 400;
 
-  totalGeral = totalGeral + preco;
-  let valorTotal = document.getElementById("valor-total");
+function comprar() {
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+    let qtd = document.getElementById('qtd').value;
 
-  valorTotal.innerHTML = `<span class="texto-azul" id="valor-total">R$${totalGeral}</span>`;
-  document.getElementById("quantidade").value = 0;
+    alert(tipoIngresso)
+    alert(qtd)
 }
 
-function limpar() {
-    totalGeral = 0;
-    document.getElementById("lista-produtos").innerHTML = "";
-    document.getElementById("valor-total").textContent = "R$ 0";
-  }
+
+// let totalGeral = 0;
+// limpar();
+
+// function adicionar() {
+
+//   let produto = document.getElementById("produto").value;
+//   let nomeProduto = produto.split("-")[0];
+//   let valorUnitario = produto.split("R$")[1];
+//   let quantidade = document.getElementById("quantidade").value;
+
+//   let preco = quantidade * valorUnitario;
+//   let carrinho = document.getElementById("lista-produtos");
+
+//   carrinho.innerHTML =
+//     carrinho.innerHTML +
+//     `<section class="carrinho__produtos__produto">
+// <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+// </section>`;
+
+
+//   totalGeral = totalGeral + preco;
+//   let valorTotal = document.getElementById("valor-total");
+
+//   valorTotal.innerHTML = `<span class="texto-azul" id="valor-total">R$${totalGeral}</span>`;
+//   document.getElementById("quantidade").value = 0;
+// }
+
+// function limpar() {
+//     totalGeral = 0;
+//     document.getElementById("lista-produtos").innerHTML = "";
+//     document.getElementById("valor-total").textContent = "R$ 0";
+//   }
