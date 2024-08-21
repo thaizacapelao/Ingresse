f91861d36ee4a2f96f9d9d72621658e12dd459e9
Thaiza Capelão <thaizacapelao@gmail.com>
2024-08-21 17:23:44 -0400

    att

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
-
-let qntPista = 100;
 let qntCadeiraSuperior = 200;
 let qntCadeiraInferior = 400;
 
 function comprar() {
-    let tipoIngresso = document.getElementById('tipo-ingresso').value;
-    let qtd = document.getElementById('qtd').value;
+    let tipoIngresso = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
 
-    alert(tipoIngresso)
-    alert(qtd)
+    if(tipoIngresso.value == 'pista') {
+        comprarPista(qtd);
+        }
 }
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if(qtd > qtdPista) {
+        alert('Quantidade de ingressos indisponível para a pista!');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com Sucesso.');
+    }
+ }
+
