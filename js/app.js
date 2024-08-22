f5f94e5af594a9ee4f89602563118a09cabe2824
Thaiza Capelão <thaizacapelao@gmail.com>
2024-08-22 11:47:37 -0400

    att

--- js/app.js
+++ js/app.js
@@ -1,23 +1,49 @@
+// let pista = 100;
 // let qntCadeiraSuperior = 200;
 // let qntCadeiraInferior = 400;
 
 function comprar() {
+    let qtdCompra = parseInt(document.getElementById('qtd').value);
     let tipoIngresso = document.getElementById('tipo-ingresso');
-    let qtd = parseInt(document.getElementById('qtd').value);
 
-    if(tipoIngresso.value == 'pista') {
-        comprarPista(qtd);
-        }
+    if (tipoIngresso.value == "pista") {
+        comprarPista(qtdCompra);
+    } else  if (tipoIngresso.value == "superior") {
+        comprarSuperior(qtdCompra);
+    } else  if (tipoIngresso.value == "inferior") {
+        comprarInferior(qtdCompra);
+    }
 }
 
 function comprarPista(qtd) {
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
-    if(qtd > qtdPista) {
-        alert('Quantidade de ingressos indisponível para a pista!');
-    } else {
+      if (qtd > qtdPista) {
+        alert("Quantidade de ingressos indisponível para a pista!");
+      } else {
         qtdPista = qtdPista - qtd;
-        document.getElementById('qtd-pista').textContent = qtdPista;
-        alert('Compra realizada com Sucesso.');
-    }
+        document.getElementById("qtd-pista").textContent = qtdPista;
+        alert("Compra realizada com Sucesso.");
+      }
  }
 
+ function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+      if (qtd > qtdSuperior) {
+        alert("Quantidade de ingressos indisponível para a Superior!");
+      } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById("qtd-superior").textContent = qtdSuperior;
+        alert("Compra realizada com Sucesso.");
+      }
+ }
+
+ function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+      if (qtd > qtdInferior) {
+        alert("Quantidade de ingressos indisponível para a Superior!");
+      } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById("qtd-inferior").textContent = qtdInferior;
+        alert("Compra realizada com Sucesso.");
+      }
+ }
